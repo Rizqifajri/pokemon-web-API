dade0842c0fe8ea885acf9351eacb2399771ac56
Rizqi Fajri <kikifajri08@gmail.com>
2023-08-30 02:30:59 +0700

    second commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,23 @@
 import React from 'react'
-import {FaGithub} from 'react-icons/fa'
-// import {MdCatchingPokemon} from 'react-icons/md'
 import bolapoke from '../assets/bolapoke.png'
 import logopoke from '../assets/logopoke.png'
-
+import face from '../assets/piface.png'
 
 const Header = () => {
   return (
     <div className='bg-yellow-400 min-h-10 py-2 flex justify-between px-5 fixed w-full top-0' >
       
-      <div className='w-10'>
-      <img src={bolapoke} className=""/>
-      </div>
-    <div className='w-24'>
-    <img src={logopoke} alt="" />
+    <div className='w-12 animate-bounce mt-5'>
+        <img src={bolapoke} className=""/>
     </div>
+    <div className='w-32'>
+        <img src={logopoke} alt="" />
+    </div>
+    
+    
 
-      <FaGithub size={35} />
+      
+    
      
      
     </div>
